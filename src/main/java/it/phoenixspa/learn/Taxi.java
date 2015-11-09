a5cdc5195dfbc43636f1a3dc0b1dcfe42588c1b4
Pietro Di Bello <pietro.dibello@xpeppers.com>
2015-11-09 16:58:05 +0100

    Taxi reimplements calcolaBollo behaviour

--- src/main/java/it/phoenixspa/learn/Taxi.java
+++ src/main/java/it/phoenixspa/learn/Taxi.java
@@ -9,6 +9,11 @@ public class Taxi extends Car {
         this.license = license;
     }
 
+    @Override
+    public Double calcolaBollo() {
+        return super.calcolaBollo() / 2;
+    }
+
     void renewLicense() {
         if (license.startsWith("2014")) {
             license = "2015" + license;
